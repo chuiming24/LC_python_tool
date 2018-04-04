ea20ea6e60243c0d454f2f4e896a6cd54f03d502
二四 <794960040@qq.com>
2018-04-04 16:31:20 +0800

    更新依赖库批处理文件

---
+++ lost/V0.11.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*- 
+from tkinter import *
+import webbrowser
+import pyperclip
+import threading as mp
+import ctypes
+import time
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+
+
+#获取剪切板内容
+def gettext():
+    return pyperclip.paste()
+
+
+
+def getTextb(a):
+    print("剪切板监督中")
+    loatText = ''
+    while(True):
+        #getText = gettext()
+        sleep(0.2)
+        if(len(getText) != 0 and loatText != getText and (getText[0] == 'C' or getText[0] == 'C' )):
+            sleep(0.2)
+            loatText = getText
+            webbrowser.open('http://so.szlcsc.com/global.html?c=&k='+getText)
+    
+
+
+class App:
+
+    def __init__(self, master):
+        master.withdraw()
+        Popup = Toplevel(master)
+        #Popup.resizable(0,0)
+        #Popup.attributes("-toolwindow", 1)
+        Popup.wm_attributes("-topmost", 1)
+
+        Popup.overrideredirect(True)
+        frame = Frame(Popup)
+        frame.pack()
+
+        #self.p1 = mp.Thread(target = getTextb, args = (1,))
+
+        self.flag = True
+
+        self.input = Entry(frame)
+        self.input.pack(side=TOP)
+        self.input.bind('<Key-Return>', self.say_seach)
+        
+        self.button = Button(
+            frame, text="退出", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="查找商品", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+        #self.hi_there = Button(frame, text="监督剪切板", command=self.p1.start())
+        #self.hi_there.pack(side=LEFT)
+        
+        self.hi_there = Button(frame, text="新后台", command=self.say_newadmin)
+        self.hi_there.pack(side=LEFT)
+        
+        self.hi_there = Button(frame, text="编号网页", command=self.say_codehtml)
+        self.hi_there.pack(side=LEFT)        
+
+    def say_hi(self):
+        print("hi there, everyone!")
+        print(self.input.get())
+        webbrowser.open('http://so.szlcsc.com/global.html?c=&k='+self.input.get())
+
+    def say_seach(self, event):
+        self.say_codehtml()
+
+
+    def say_newadmin(self):
+        webbrowser.open('https://erp.szlcsc.com/#/')
+
+    def say_codehtml(self):
+        self.flag = True
+        count = 0;
+        urlf = ''
+        url = 'http://so.szlcsc.com/global.html?c=&k='+self.input.get()
+        r = requests.get(url)
+        html = r.text
+        #html = urlopen(url).read().decode('gbk')
+        soup = BeautifulSoup(html, features='lxml')
+        
+        all_href = ''
+        for link in soup.find_all('a'):
+            all_href = link.get('href')
+            if(type(all_href) == str):
+                if 'item' in all_href:
+                    urlf = all_href
+                    
+                    print(all_href)
+                    self.flag = False
+                    break
+                
+        for link in soup.find_all('a'):
+            all_href = link.get('href')                
+            if(type(all_href) == str):
+                if 'item' in all_href:
+                    count = count + 1                
+                if(count > 4):
+                    self.flag = True
+                    break
+            
+        if(self.flag):     
+            self.say_hi()
+            print(self.input.get())
+        else:
+            webbrowser.open(urlf) 
+        
+        
+        
+
+if __name__=='__main__':
+    
+    whnd = ctypes.windll.kernel32.GetConsoleWindow()  
+    if whnd != 0:  
+        ctypes.windll.user32.ShowWindow(whnd, 0)  
+        ctypes.windll.kernel32.CloseHandle(whnd)  
+    root = Tk()
+    app = App(root)
+
+    root.mainloop()
+    root.destroy() # optional; see description below
---
+++ 安装依赖库.cmd
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip
+pip install pyperclip
+pip install beautifulsoup4
+pip install lxml	
+pip install requests
+pause
