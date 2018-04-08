37df1439666731b2676c98aa639018fb516a8d5f
chuiming24 <794960040@qq.com>
2018-04-09 07:27:23 +0800

    点击实验

--- text/python_selenium/01text.py
+++ text/python_selenium/01text.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
- 
+from bs4 import BeautifulSoup
+import time
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-print(driver.page_source)
+driver.maximize_window() 
+driver.get("http://list.szlcsc.com/catalog/582.html")
+#assert "Python" in driver.title
+#elem = driver.find_element_by_name("q")
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+html = driver.page_source
+soup = BeautifulSoup(html, features='lxml')
+for link in soup.find_all('a'):
+    all_href = link.get('href')
+    if(type(all_href) == str):
+        if 'item' in all_href:
+            print(all_href)
+# 滚动条置底  
+js1 = "var q=document.documentElement.scrollTop=1000000"  
+driver.execute_script(js1)  
+#driver.find_element_by_class_name("next").click()
+#time.sleep(5)
+#driver.implicitly_wait(30)
+#driver.find_element_by_class_name("next").click()
 #https://www.cnblogs.com/BigFishFly/p/6380024.html
-driver.close()
+#https://blog.csdn.net/liuchunming033/article/details/46789085
+
+#driver.close()
